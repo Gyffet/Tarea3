a43a6535ee8fb870624a0655a9991f497ca5a643
Gyffet <66542420+Gyffet@users.noreply.github.com>
2022-03-25 15:09:50 -0600

    Update ServidorRMIP5.java

--- ServidorRMIP5.java
+++ ServidorRMIP5.java
@@ -2,7 +2,7 @@ import java.rmi.Naming;
 
 public class ServidorRMIP5 {
     public static void main(String[] args) throws Exception {
-        String url = "rmi://10.0.0.4/Tarea5";
+        String url = "rmi://localhost/Tarea5";
         ClaseRMIP5 obj = new ClaseRMIP5();
         
         //registra una instancia en el rmiregistry
